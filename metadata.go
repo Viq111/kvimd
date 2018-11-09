f0b1aca70c7db2e54af452e31df074a4262b2b9d
Vianney Tran <vianney.tran@datadoghq.com>
2018-11-09 08:36:48 -0500

    [metadata] Update
    This file might go away though as it is not useful

--- metadata.go
+++ metadata.go
@@ -3,6 +3,6 @@ package kvimd
 // Metadata is a datastructure given information about the initial creation of the DB
 // It is a (json) exportable data structure that's written to metadata
 type Metadata struct {
-	workers  int `json:"workers"`
-	hashSize int `json:"hash_size"` // The size of each hashmap file
+	workers  int    `json:"workers"`
+	hashSize uint32 `json:"hash_size"` // The size of each hashmap file
 }
